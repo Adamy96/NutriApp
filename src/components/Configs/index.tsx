84295d3cd4564d453004ceb81129fdaad40196b2
Adamy96 <vinicius.adamy@globalhitss.com.br>
2022-05-16 10:19:35 -0300

    Add google analytics

--- src/components/Configs/index.tsx
+++ src/components/Configs/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import Script from 'next/script'
 import {
   Chart as ChartJS,
   ArcElement,
@@ -41,7 +42,22 @@ const Configs = () => {
     }
   }, [currentTheme])
   
-  return null
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
+  )
 }
 
 export default Configs
